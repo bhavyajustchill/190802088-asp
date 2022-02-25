73f9b908f7f45380fad97df8b6b42c029496d943
Bhavya Popat <bhavyabpopat7777@gmail.com>
2022-02-25 11:50:20 +0530

    incomplete v1.0.0

--- Login.aspx.cs
+++ Login.aspx.cs
@@ -7,8 +7,13 @@ using System.Web.UI.WebControls;
 
 public partial class Login : System.Web.UI.Page
 {
+    //DeleteCommand="DELETE FROM [users] WHERE [id] = @id" 
+    //InsertCommand="INSERT INTO [users] ([fullname], [email], [password]) VALUES (@fullname, @email, @password)" 
+    //ProviderName="<%$ ConnectionStrings:DatabaseConnectionString1.ProviderName %>" 
+    //SelectCommand="SELECT [id], [fullname], [email], [password] FROM [users]" 
+    //UpdateCommand="UPDATE [users] SET [fullname] = @fullname, [email] = @email, [password] = @password WHERE [id] = @id"
     protected void Page_Load(object sender, EventArgs e)
     {
-        
+
     }
 }
--- src/js/morris.js
+++ src/js/morris.js
@@ -1,28 +1,3 @@
-/*Copyright (c) 2013, Olly Smith
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-1. Redistributions of source code must retain the above copyright notice, this
-list of conditions and the following disclaimer.
-2. Redistributions in binary form must reproduce the above copyright notice,
-this list of conditions and the following disclaimer in the documentation
-and/or other materials provided with the distribution.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
-ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
-
-
-
 (function(){var $,Morris,minutesSpecHelper,secondsSpecHelper,__slice=[].slice,__bind=function(fn,me){return function(){return fn.apply(me,arguments);};},__hasProp={}.hasOwnProperty,__extends=function(child,parent){for(var key in parent){if(__hasProp.call(parent,key))child[key]=parent[key];}function ctor(){this.constructor=child;}ctor.prototype=parent.prototype;child.prototype=new ctor();child.__super__=parent.prototype;return child;},__indexOf=[].indexOf||function(item){for(var i=0,l=this.length;i<l;i++){if(i in this&&this[i]===item)return i;}return-1;};Morris=window.Morris={};$=jQuery;Morris.EventEmitter=(function(){function EventEmitter(){}
 EventEmitter.prototype.on=function(name,handler){if(this.handlers==null){this.handlers={};}
 if(this.handlers[name]==null){this.handlers[name]=[];}
